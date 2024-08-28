c2f59bee3bd1ec9dfbfde48d2e606ba66c420f76
sushanta Das <sushanta@easyrewardz.com>
2024-08-28 20:26:20 +0530

    system activity

---
+++ Pages/CheckStatus.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Time_Activity.Pages
+{
+    public class CheckStatusModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
